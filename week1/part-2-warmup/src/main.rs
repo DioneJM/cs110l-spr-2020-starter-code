bd94c231a1ade70b4df13273cf50b76281faeb1f
Dione <dionemorales@outlook.com>
2021-02-28 19:07:36 +1100

    solutions to week 1 part 2 exercises

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -7,15 +7,34 @@ fn main() {
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    v.iter().map(|&x| x + n).collect()
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for num in v.iter_mut() {
+        *num = *num + n
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut dup_nums: HashSet<i32> = HashSet::new();
+    let mut indices_to_remove: Vec<usize> = Vec::new();
+    for (index, num) in v.iter_mut().enumerate() {
+        if dup_nums.contains(num) {
+            indices_to_remove.push(index);
+        } else {
+            dup_nums.insert(*num);
+        }
+    }
+
+    for (cur_index, index_to_remove) in indices_to_remove.iter().enumerate() {
+        v.remove(*index_to_remove - cur_index);
+    }
+}
+
+fn dedup_simplified(v: &mut Vec<i32>) {
+    let mut uniques: HashSet<i32> = HashSet::new();
+    v.retain(|x| uniques.insert(*x))
 }
 
 #[cfg(test)]
@@ -25,6 +44,7 @@ mod test {
     #[test]
     fn test_add_n() {
         assert_eq!(add_n(vec![1], 2), vec![3]);
+        assert_eq!(add_n(vec![1,2,3], 2), vec![3, 4, 5]);
     }
 
     #[test]
